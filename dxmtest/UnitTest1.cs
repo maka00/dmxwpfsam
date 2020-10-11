091bc6a3615d75003ef885e3584f6466468832c9
maka00 <34419221+maka00@users.noreply.github.com>
2020-10-11 10:46:19 +0200

    move to xunit test framework

--- dxmtest/UnitTest1.cs
+++ dxmtest/UnitTest1.cs
@@ -1,18 +1,14 @@
-using NUnit.Framework;
+using System;
+using Xunit;
 
 namespace dxmtest
 {
-    public class Tests
+    public class UnitTest1
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
+        [Fact]
         public void Test1()
         {
-            Assert.Pass();
+          Assert.True(true, "should be true");
         }
     }
 }
