450990fdeba48df0ede7405aaf2bed606bb962b8
maka00 <34419221+maka00@users.noreply.github.com>
2020-10-11 13:10:20 +0200

    add user control

---
+++ dxm/ViewModel/UCFlow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dxm.ViewModel
+{
+    /// <summary>
+    /// Interaction logic for UCFlow.xaml
+    /// </summary>
+    public partial class UCFlow : UserControl
+    {
+        public UCFlow()
+        {
+            InitializeComponent();
+        }
+    }
+}
